44b59a43364ee7a0d3c3bec367c295568f968d15
Kembi0736 <125196600+Kembi0736@users.noreply.github.com>
2026-04-04 22:45:23 +1000

    Multiple changes
    - Added FOV
    - Player can access all rooms (Kruskal algorithm)
    - Added interface (Status and event log)
    - Player control in russian (цфыв)
    - Player's start position now random

--- map.py
+++ map.py
@@ -1,6 +1,5 @@
-from random import choice
-from player import Player
-from file import Vector2
+from random import choice, shuffle, randint
+from player import Player, Vector2, Interface
 import keyboard
 import os
 
@@ -11,13 +10,15 @@ class Map:
         self.room_width = room_width
         self.map_height = map_height
         self.map_width = map_width
-        self.tiles = ['\033[90m#\033[0m', '\033[92m.\033[0m', '\033[92m.\033[0m', '\033[92m.\033[0m', '\033[94m~\033[0m']
-        self.not_walkable_tiles = ['\033[90m#\033[0m', '┌', '─', '┐', '│', '└', '┘']
+        self.map = {}
+        self.visited_rooms = {}
+        self.tiles = ['\033[90mΩ\033[0m', '\033[92m.\033[0m', '\033[92m.\033[0m', '\033[92m.\033[0m', '\033[94m◦\033[0m']
+        self.not_walkable_tiles = ['┌', '─', '┐', '│', '└', '┘']
         
 
     def generate_room(self):
         room = []
-
+        
         for y in range(self.room_height):
             row = []
             for x in range(self.room_width):
@@ -44,8 +45,40 @@ class Map:
         return room
     
 
+    def generate_map(self):
+        area = self.map_height * self.map_width
+        self.map = {i: set() for i in range(1, area + 1)}
+        edges = []
+
+        for i in range(1, area + 1):
+            if i % self.map_width != 0:
+                edges.append((i, i + 1))
+
+            if i + self.map_width <= area:
+                edges.append((i, i + self.map_width))
+
+        shuffle(edges)
+        parent = list(range(area + 1))
+
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+
+            return parent[x]
+        
+
+        for a, b in edges:
+            if find(a) != find(b):
+                parent[find(a)] = find(b)
+                self.map[a].add(b)
+                self.map[b].add(a)
+
+
     def generate_global_map(self):
         rooms = []
+        self.generate_map()
+        shtuka = self.map
 
         for y in range(self.map_height):
             row = []
@@ -53,15 +86,20 @@ class Map:
                 room = self.generate_room()
                 center_x = self.room_width // 2
                 center_y = self.room_height // 2
+                room_order = y*self.map_width + x + 1
 
-                if y < 2:
-                    room[self.room_height - 1][center_x] = ' '
-                if y > 0:
-                    room[0][center_x] = ' '
-                if x < 2:
-                    room[center_y][self.room_width - 1] = ' '
-                if x > 0:
-                    room[center_y][0] = ' '
+                for i in shtuka[room_order]:
+                    if abs(room_order - i) == 1:
+                        if i > room_order:
+                            room[center_y][self.room_width - 1] = '■'
+                        else:
+                            room[center_y][0] = '■'
+                    elif abs(room_order - i) == self.map_width:
+                        if i > room_order:
+                            room[self.room_height - 1][center_x] = '█'
+                        else:
+                            room[0][center_x] = '█'
+                    
 
                 row.append(room)
             rooms.append(row)
@@ -75,7 +113,7 @@ class Map:
         local_x = position.x % self.room_width
         local_y = position.y % self.room_height
 
-        if room_x < 0 or room_x >= 3 or room_y < 0 or room_y >= 3:
+        if room_x < 0 or room_x >= self.map_width or room_y < 0 or room_y >= self.map_height:
             return False
         
         return rooms[room_y][room_x][local_y][local_x] not in self.not_walkable_tiles
@@ -86,63 +124,76 @@ class Map:
         player.position.y = room_y * self.room_height + self.room_height // 2
 
     
-    def draw_map(self, player: Player, rooms):
+    def draw_map(self, player: Player, rooms, interface: Interface):
         room_x = player.position.x // self.room_width  # Координаты комнаты на карте
         room_y = player.position.y // self.room_height
         local_x = player.position.x % self.room_width  # Координаты игрока в комнате
         local_y = player.position.y % self.room_height
+
+        if (room_x, room_y) not in self.visited_rooms:
+            room_copy = [row[:] for row in rooms[room_y][room_x]]
+            self.visited_rooms[(room_x, room_y)] = room_copy
+
+        side_lines = interface.get_lines()
+        map_lines = []
 
         for room_row in range(self.map_height):
             for y in range(self.room_height):
                 line = []
 
                 for room_column in range(self.map_width):
-                    room = rooms[room_row][room_column]
+                    if (room_column, room_row) in self.visited_rooms:
+                        room = rooms[room_row][room_column]
 
-                    if (room_row == room_y and room_column == room_x and y == local_y):
-                        row = list(room[y])
-                        row[local_x] = '\033[93m@\033[0m'
-                        line.append(''.join(row))
+                        if (room_row == room_y and room_column == room_x and y == local_y):
+                            row = list(room[y])
+                            row[local_x] = '\033[93m☺\033[0m'
+                            line.append(''.join(row))
+                        else:
+                            line.append(''.join(room[y]))
                     else:
-                        line.append(''.join(room[y]))
+                        line.append('\033[30m' + '█' * self.room_width + '\033[0m')
 
                     if room_column < 2:
-                        line.append('  ')
+                        line.append('')
+                # print(" " * int(self.room_width*0.5), end = "")
+                map_lines.append(''.join(line))
 
-                print(''.join(line))
-
-            if room_row < 2:
-                separator = []
-                for room_col in range(self.map_width):
-                    separator.append(' ' * self.room_width)
-                    if room_col < 2:
-                        separator.append('   ')
-                print(''.join(separator))
+        max_lines = max(len(map_lines), len(side_lines))
+    
+        for i in range(max_lines):
+            map_part = map_lines[i] if i < len(map_lines) else ' ' * len(map_lines[0]) if map_lines else ''
+            side_part = side_lines[i] if i < len(side_lines) else ''
+            
+            print(f"{map_part}{' ' * self.room_width}{side_part}")
 
 
-game_map = Map(map_height=3, map_width=3)
+
+game_map = Map(map_height=5, map_width=4)
 rooms = game_map.generate_global_map()
 
-player = Player(Vector2(0, 0), 10, 2)
-game_map.set_player(player, 0, 0)
+player = Player(Vector2(0, 0), 20, 20, 2, 10)
+interface = Interface(player)
+game_map.set_player(player, randint(0, game_map.map_width - 1), randint(0, game_map.map_height - 1))
+
 
 while True:
     os.system("cls")
-    game_map.draw_map(player, rooms)
+    game_map.draw_map(player, rooms, interface)
     event = keyboard.read_event()
     
     if event.event_type == keyboard.KEY_DOWN:
         match event.name:
-            case 'w':
+            case 'w' | 'ц':
                 if game_map.is_walkable(player.position + Vector2(0, -1), rooms):
                     player.move(Vector2(0, -1))
-            case 's':
+            case 's' | 'ы':
                 if game_map.is_walkable(player.position + Vector2(0, 1), rooms):
                     player.move(Vector2(0, 1))
-            case 'a':
+            case 'a' | 'ф':
                 if game_map.is_walkable(player.position + Vector2(-1, 0), rooms):
                     player.move(Vector2(-1, 0))
-            case 'd':
+            case 'd' | 'в':
                 if game_map.is_walkable(player.position + Vector2(1, 0), rooms):
                     player.move(Vector2(1, 0))
             case 'esc':
--- player.py
+++ player.py
@@ -1,10 +1,53 @@
-from file import Vector2
+class Vector2:
+    __slots__ = ('x', 'y')
 
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+    
+    def normalize(self):
+        absolute = abs(self)
+        self.x /= absolute
+        self.y /= absolute
+        self.z /= absolute
+        return Vector2(self.x / absolute, self.y / absolute)
 
+    def __contains__(self, item):
+        return item in [self.x, self.y]
+
+    def __repr__(self):
+        return f"X: {self.x} Y: {self.y}"
+    
+    @staticmethod
+    def up(direction = -1):
+        return Vector2(0, -1*direction)
+
+    @staticmethod
+    def right(direction = 1):
+        return Vector2(1*direction, 0)
+
+    def __eq__(self, value):
+        return self.x == value.x and self.y == value.y
+
+    def __bool__(self):
+        return Vector2(0, 0) != self
+        
+    def __add__(self, vec):
+        return Vector2(self.x + vec.x, self.y + vec.y)
+    
+    def __sub__(self, vec):
+        return Vector2(self.x - vec.x, self.y - vec.y)
+
+    def __abs__(self):
+        return (self.x**2 + self.y**2)**0.5
+    
+    
 class Player:
-    def __init__(self, coords: Vector2, health: int, armor: int):
+    def __init__(self, coords: Vector2, health: int, max_health: int, armor: int, max_armor: int):
         self.position = coords
         self.health = health
+        self.max_health = max_health
+        self.max_armor = max_armor
         self.armor = armor
 
     def move(self, direction: Vector2):
@@ -17,6 +60,72 @@ class Enemy(Player):
         super().__init__(coords, health, armor)
 
 
-class Inventory:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
+class Interface:
+    def __init__(self, player: Player):
+        self.player = player
+        self.width = 50
+        self.height = 20
+
+
+    # def __call__(self):
+    #     hp_color = '\033[91m' if self.player.health < (self.player.health * 0.4) else '\033[92m'
+    #     armor_color = '\033[94m'
+    #     reset = '\033[0m'
+        
+    #     hp_bar = f"{hp_color}{'█' * self.player.health}{'░' * (10 - self.player.health)}{reset}"
+    #     armor_bar = f"{armor_color}{'█' * self.player.armor}{'░' * (10 - self.player.armor)}{reset}"
+        
+    #     print(f"\r{hp_bar} ❤️  {armor_bar} 🛡️  ({self.player.position.x},{self.player.position.y})", end="")
+
+
+    def get_lines(self):
+        lines = []
+
+        lines.append(f"┌{'─' * (self.width - 2)}┐")
+        lines.append(f"│{"Статус":^{self.width - 2}}│")
+        lines.append(f"├{'─' * (self.width - 2)}┤")
+
+        hp_color = '\033[91m' if self.player.health < (self.player.max_health * 0.4) else '\033[92m'
+        reset = '\033[0m'
+        hp_text = f"❤️  HP: {self.player.health}/{self.player.max_health}"
+        hp_bar = f"{hp_color}{'█' * self.player.health}{'░' * (self.player.max_health - self.player.health)}{reset}"
+        lines.append(f"│ {hp_text:<{self.width - 4}} │")
+        lines.append(f"│ {hp_bar:<{self.width + 5}} │")
+
+        armor_text = f"🛡️  ARMOR: {self.player.armor}/{self.player.max_armor}"
+        armor_bar = f"\033[94m{'█' * self.player.armor}{'░' * (self.player.max_armor - self.player.armor)}\033[0m"
+        lines.append(f"│ {armor_text:<{self.width - 4}} │")
+        lines.append(f"│ {armor_bar:<{self.width + 5}} │")
+
+        lines.append(f"│ {"🎮  LEVEL: ":<{self.width - 5}} │")
+
+        lines.append(f"├{'─' * (self.width - 2)}┤")
+        lines.append(f"│{"Журнал собыйтий":^{self.width - 2}}│")
+        lines.append(f"├{'─' * (self.width - 2)}┤")
+
+        for _ in range(self.height):    # Ну вроде заглушка
+            lines.append(f"│{' ' * (self.width - 2)}│")
+
+        lines.append(f"└{'─' * (self.width - 2)}┘")
+
+        return lines
+
+
+
+# class Inventory:
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.items = []
+
+
+#     def add(self, item):
+#         self.items.append(item)
+
+
+#     def show(self):
+#         if not self.items:
+#             print("Инвентарь пуст")
+#             return
+        
+#         for i, item in enumerate(self.items, 1):
+#             print(i, item)
